e89adc58279906bca3236267111ec75a39a628ea
JD <jherlehy@uwo.ca>
2021-09-02 11:24:13 -0400

    Added Cruise Control

--- Saddle_Main/Saddle_Main/Saddle_Main.ino
+++ Saddle_Main/Saddle_Main/Saddle_Main.ino
@@ -7,6 +7,7 @@
  * J.D. Herlehy
  * Octavio Daiz
  * To drive and control the saddle
+ * 2021.9.2 + Added Cruise Control JDH
  */
 
 #define ButtonOne 33
@@ -18,6 +19,13 @@
 #define XAxis 4
 #define YAxis 14
 #define Power 35 //subject to change to real pinout
+
+//define other vars
+int cruise; //setting if it's on or off
+int cruiseL; //value of cruise left duty %
+int cruiseLD; //value of cruise left direction
+int cruiseR; //value of cruise right duty %
+int cruiseRD; //value of cruise right direction
 
 
 void setup() {
@@ -51,7 +59,42 @@ void loop() {
     //fetch input from axis and output to the motors
     //hold the output motor values for use in cruise control (secondary feature)
     //use an array to hold motor values or use same method twice but return different things
+    //implement the conversion into this main code?
     
+    //Cruise Control
+    //If a button is pressed hold the current motor outputs
+    //Publish the held output to the motors until the other button is pressed
+    //otherwise publish the real time outputs
+    //set casses, one case for non cruise and one case for cruise, check case number
+
+    if(analogRead(ButtonOne) == 0){ //0 might be unpressed, change to != otherwise
+      cruise = 1;
+      cruiseL = leftMotor;
+      cruiseLD = leftMotorD;
+      cruiseR = rightMotor;
+      cruiseRD = rightMotorD;
+    }
+
+    switch(cruise){
+      case 0: //non-cruise case
+        ledcWrite(6, leftMotor);
+        ledcWrite(10, rightMotor);
+        digitalWrite(DIRLeft, leftMotorD);
+        digitalWrite(DIRRight, rightMotorD);
+
+        break;
+        
+      case 1: //cruise case
+        if(analogRead(ButtonTwo) == 0){ //0 might be unpressed, change to != otherwise
+          cruise = 0;
+        }
+        ledcWrite(6, cruiseL);
+        ledcWrite(10, cruiseR);
+        digitalWrite(DIRLeft, cruiseLD);
+        digitalWrite(DIRRight, cruiseRD);
+
+        break;
+    }
     
   }
   
